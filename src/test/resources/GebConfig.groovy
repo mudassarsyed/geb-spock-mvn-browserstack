29636ecb4a7c1bc121c66615ce93ef4964bfb77e
mudassarsyed <52317628+mudassarsyed@users.noreply.github.com>
2019-10-23 15:04:42 +0530

    Update GebConfig.groovy

--- src/test/resources/GebConfig.groovy
+++ src/test/resources/GebConfig.groovy
@@ -12,7 +12,7 @@ import geb.driver.BrowserStackDriverFactory
             assert username
             def accessKey = "8ZezGR6GyZjP2ZqrXphh"
             assert accessKey
-            new BrowserStackDriverFactory().create("", username, accessKey, ['build': 'mudassar', 'browser':'Chrome','os': 'Windows','os_version': '8','browser_version': '77.0','browserstack.local': 'false'])
+            new BrowserStackDriverFactory().create("", username, accessKey, ['build': 'mudassar', 'browser':'Chrome','os': 'Windows','os_version': '8','browser_version': '77.0','browserstack.local': 'true'])
 
         }
 baseUrl = "http://google.com"
