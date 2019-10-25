de9937aa7ab7cb85d1e0382fd1c72c8f6043a23d
mudassarsyed <52317628+mudassarsyed@users.noreply.github.com>
2019-10-25 19:18:29 +0530

    Update GebConfig.groovy

--- src/test/resources/GebConfig.groovy
+++ src/test/resources/GebConfig.groovy
@@ -17,6 +17,7 @@ driver = {
     def browserstackLocal = System.getenv("BROWSERSTACK_LOCAL");
     def browserstackLocalIdentifier = System.getenv("BROWSERSTACK_LOCAL_IDENTIFIER");
     new BrowserStackDriverFactory().create(username, accessKey,['build': 'mudassar', 'browser':'firefox','os': 'Windows','os_version': '8','browser_version': '69','browserstack.local': browserstackLocal,"browserstack.localIdentifier":browserstackLocalIdentifier])
+    break
 }
 case "chrome" :
 driver = {
@@ -27,6 +28,7 @@ driver = {
     def browserstackLocal = System.getenv("BROWSERSTACK_LOCAL");
     def browserstackLocalIdentifier = System.getenv("BROWSERSTACK_LOCAL_IDENTIFIER");
     new BrowserStackDriverFactory().create(username, accessKey,['build': 'mudassar', 'browser':'chrome','os': 'Windows','os_version': '8','browser_version': '76','browserstack.local': browserstackLocal,"browserstack.localIdentifier":browserstackLocalIdentifier])
+    break
 }
 }
 
