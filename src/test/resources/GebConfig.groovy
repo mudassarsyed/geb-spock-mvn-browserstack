2a9396d8c60897c7bb668b9fbce2e849f224fca2
mudassarsyed <52317628+mudassarsyed@users.noreply.github.com>
2019-10-25 16:57:27 +0530

    Update GebConfig.groovy

--- src/test/resources/GebConfig.groovy
+++ src/test/resources/GebConfig.groovy
@@ -5,7 +5,7 @@
 */
 
 import geb.driver.BrowserStackDriverFactory
-def chrome = '{"build":"mudassar","browser":"Chrome","os":"Windows""os_version":"8","browser_version":"77.0"}'
+def firefox = ['build': 'mudassar', 'browser':'firefox','os': 'Windows','os_version': '8','browser_version': '69','browserstack.local': browserstackLocal,"browserstack.localIdentifier":browserstackLocalIdentifier]
 
 driver = {
     def username = System.getenv("BROWSERSTACK_USERNAME")
@@ -14,7 +14,7 @@ driver = {
     assert accessKey
     def browserstackLocal = System.getenv("BROWSERSTACK_LOCAL");
     def browserstackLocalIdentifier = System.getenv("BROWSERSTACK_LOCAL_IDENTIFIER");
-    new BrowserStackDriverFactory().create("", username, accessKey, ['build': 'mudassar', 'browser':'firefox','os': 'Windows','os_version': '8','browser_version': '69','browserstack.local': browserstackLocal,"browserstack.localIdentifier":browserstackLocalIdentifier])
+    new BrowserStackDriverFactory().create("", username, accessKey, ${firefox})
 
 }
 baseUrl = "http://google.com"
