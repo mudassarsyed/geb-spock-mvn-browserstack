c04992256963de68b1b4abfe8c70d2e58df5f6db
mudassarsyed <52317628+mudassarsyed@users.noreply.github.com>
2019-10-25 15:48:54 +0530

    Update GebConfig.groovy

--- src/test/resources/GebConfig.groovy
+++ src/test/resources/GebConfig.groovy
@@ -7,18 +7,6 @@
 import geb.driver.BrowserStackDriverFactory
 def chrome = '{"build":"mudassar","browser":"Chrome","os":"Windows""os_version":"8","browser_version":"77.0"}'
 
-
-
-        driver = {
-            def username = System.getenv("BROWSERSTACK_USERNAME")
-            assert username
-            def accessKey = System.getenv("BROWSERSTACK_ACCESS_KEY")
-            assert accessKey
-	def browserstackLocal = System.getenv("BROWSERSTACK_LOCAL");
-	def browserstackLocalIdentifier = System.getenv("BROWSERSTACK_LOCAL_IDENTIFIER");
-        new BrowserStackDriverFactory().create("", username, accessKey, ['build': chrome.build, 'browser':Chrome.browser,'os': chrome.os,'os_version': chrome.os_version,'browser_version': chrome.browser_version,'browserstack.local': browserstackLocal,"browserstack.localIdentifier":browserstackLocalIdentifier])
-
-        }
 driver = {
     def username = System.getenv("BROWSERSTACK_USERNAME")
     assert username
