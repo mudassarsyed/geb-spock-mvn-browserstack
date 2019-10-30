6918c2039b6e06b6a37bb26bdff236adeac53f63
mudassarsyed <52317628+mudassarsyed@users.noreply.github.com>
2019-10-30 18:15:05 +0530

    Update GebConfig.groovy

--- src/test/resources/GebConfig.groovy
+++ src/test/resources/GebConfig.groovy
@@ -16,7 +16,7 @@ driver = {
     assert accessKey
     def browserstackLocal = System.getenv("BROWSERSTACK_LOCAL");
     def browserstackLocalIdentifier = System.getenv("BROWSERSTACK_LOCAL_IDENTIFIER");
-    new BrowserStackDriverFactory().create(username, accessKey,['build': 'Geb-Spock-Mvn-Test', 'browser':'firefox','os': 'Windows','os_version': '8','browser_version': '69','browserstack.local': browserstackLocal,"browserstack.localIdentifier":browserstackLocalIdentifier])
+    new BrowserStackDriverFactory().create(username, accessKey,['build': 'Geb-Spock-Mvn-Test','name' : 'single', 'browser':'firefox','os': 'Windows','os_version': '8','browser_version': '69','browserstack.local': browserstackLocal,"browserstack.localIdentifier":browserstackLocalIdentifier])
 }
    break
 case "chrome" :
@@ -27,7 +27,7 @@ driver = {
     assert accessKey
     def browserstackLocal = System.getenv("BROWSERSTACK_LOCAL");
     def browserstackLocalIdentifier = System.getenv("BROWSERSTACK_LOCAL_IDENTIFIER");
-    new BrowserStackDriverFactory().create(username, accessKey,['build': 'Geb-Spock-Mvn-Test', 'browser':'chrome','os': 'Windows','os_version': '8','browser_version': '76','browserstack.local': browserstackLocal,"browserstack.localIdentifier":browserstackLocalIdentifier])
+    new BrowserStackDriverFactory().create(username, accessKey,['build': 'Geb-Spock-Mvn-Test','name' : 'single', 'browser':'chrome','os': 'Windows','os_version': '8','browser_version': '76','browserstack.local': browserstackLocal,"browserstack.localIdentifier":browserstackLocalIdentifier])
 }
     break
 case "local" :
@@ -38,7 +38,7 @@ driver = {
     assert accessKey
     def browserstackLocal = System.getenv("BROWSERSTACK_LOCAL");
     def browserstackLocalIdentifier = System.getenv("BROWSERSTACK_LOCAL_IDENTIFIER");
-    new BrowserStackDriverFactory().create(username, accessKey,['build': 'Geb-Spock-Mvn-Test', 'browser':'chrome','os': 'Windows','os_version': '8','browser_version': '76','browserstack.local': 'true',"browserstack.localIdentifier":browserstackLocalIdentifier])
+    new BrowserStackDriverFactory().create(username, accessKey,['build': 'Geb-Spock-Mvn-Test','name' : 'local', 'browser':'chrome','os': 'Windows','os_version': '8','browser_version': '76','browserstack.local': 'true',"browserstack.localIdentifier":browserstackLocalIdentifier])
 }
     break
 }
