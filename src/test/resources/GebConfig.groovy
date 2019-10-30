bbe82debdfe3d84623034a7975cf97373f58f237
mudassarsyed <52317628+mudassarsyed@users.noreply.github.com>
2019-10-30 17:16:38 +0530

    Update GebConfig.groovy

--- src/test/resources/GebConfig.groovy
+++ src/test/resources/GebConfig.groovy
@@ -30,6 +30,17 @@ driver = {
     new BrowserStackDriverFactory().create(username, accessKey,['build': 'Geb-Spock-Mvn-Test', 'browser':'chrome','os': 'Windows','os_version': '8','browser_version': '76','browserstack.local': browserstackLocal,"browserstack.localIdentifier":browserstackLocalIdentifier])
 }
     break
+case "local" :
+driver = {
+    def username = System.getenv("BROWSERSTACK_USERNAME")
+    assert username
+    def accessKey = System.getenv("BROWSERSTACK_ACCESS_KEY")
+    assert accessKey
+    def browserstackLocal = System.getenv("BROWSERSTACK_LOCAL");
+    def browserstackLocalIdentifier = System.getenv("BROWSERSTACK_LOCAL_IDENTIFIER");
+    new BrowserStackDriverFactory().create(username, accessKey,['build': 'Geb-Spock-Mvn-Test', 'browser':'chrome','os': 'Windows','os_version': '8','browser_version': '76','browserstack.local': 'true',"browserstack.localIdentifier":browserstackLocalIdentifier])
+}
+    break
 }
 
 baseUrl = "http://google.com"
